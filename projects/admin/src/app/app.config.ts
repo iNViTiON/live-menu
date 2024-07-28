a8f73a50a56ec4bd310458592c1560ed486e8ebd
iNViTiON <13241662+iNViTiON@users.noreply.github.com>
2024-07-28 16:06:10 +0000

    chore: Add Firebase configuration files and dependencies

--- projects/admin/src/app/app.config.ts
+++ projects/admin/src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideFirebaseApp(() => initializeApp({ "projectId": "realtimemenuscreen", "appId": "1:180761938918:web:5d6caee839cf3368966427", "storageBucket": "realtimemenuscreen.appspot.com", "apiKey": "AIzaSyA9H30R1vabNfP8XhihQKXxm5idBKmfttA", "authDomain": "realtimemenuscreen.firebaseapp.com", "messagingSenderId": "180761938918" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())]
 };
