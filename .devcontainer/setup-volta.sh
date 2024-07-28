1ddde70ca8c85faff6ec6d3b0c242fcfb99c557f
iNViTiON <13241662+iNViTiON@users.noreply.github.com>
2024-07-28 17:52:27 +0300

    first commit

---
+++ .devcontainer/setup-volta.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+if [[ $(uname -m) == arm* || $(uname -m) == aarch* ]]; then
+  mkdir -p ~/.volta
+  cd ~/.volta
+  curl -L $(curl -sX GET https://api.github.com/repos/MyneTop/volta/releases/latest | grep browser_download_url | cut -d : -f 2,3 | tr -d '"[:blank:]') | tar -xzvf -
+  ./volta setup
+  cd -
+else
+  curl https://get.volta.sh | bash
+fi
+
+if ! grep -qF 'export PATH="$VOLTA_HOME:$PATH"' ~/.bashrc; then
+    echo 'export PATH="$VOLTA_HOME:$PATH"' >> ~/.bashrc
+fi
+
+~/.volta/bin/yarn || true
