d63919545e540d7848e780ce76e0a75f96956d02
iNViTiON <13241662+iNViTiON@users.noreply.github.com>
2024-07-29 18:34:36 +0000

    feat: Add Firebase Cloud Storage event triggers for write metadata file upload and deletion to Firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,24 @@
-import {firestore} from "firebase-admin";
+import {
+  firestore,
+  storage,
+} from "firebase-admin";
 import {initializeApp} from "firebase-admin/app";
 import * as functions from "firebase-functions";
+import * as crypto from "crypto";
+import {
+  onObjectDeleted,
+  onObjectFinalized,
+} from "firebase-functions/v2/storage";
 
-const fReg = functions.region("europe-west1");
+const region = "europe-west1";
+const fReg = functions.region(region);
 
 initializeApp();
+
+const getMd5 = (data: string) =>
+  crypto.createHash("md5")
+    .update(data)
+    .digest("hex");
 
 export const newUser = fReg.auth.user().onCreate((user) => {
   if (user.providerData.length === 0) {
@@ -25,3 +39,28 @@ export const deleteUser = fReg.auth.user().onDelete((user) => {
     [`users.${user.uid}`]: firestore.FieldValue.delete(),
   });
 });
+
+export const onFileUploaded = onObjectFinalized(
+  {region},
+  async (event) => {
+    const fileId = getMd5(event.data.id);
+    await storage().bucket(event.bucket).file(event.data.name).makePublic();
+    return firestore().collection("images").doc("images").set({
+      [fileId]: {
+        imageUrl: event.data.mediaLink,
+        index: 0,
+        time: firestore.Timestamp.now(),
+      },
+    }, {merge: true});
+  }
+);
+
+export const onFileDeleted = onObjectDeleted(
+  {region},
+  (event) => {
+    const fileId = getMd5(event.data.id);
+    return firestore().collection("images").doc("images").update({
+      [fileId]: firestore.FieldValue.delete(),
+    });
+  }
+);
